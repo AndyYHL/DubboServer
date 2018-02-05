888823b27eaf2183a02a4b1787ef963fc09d4640
yhl <love90yhl@hotmail.com>
2018-02-05 14:49:45 +0800

    集成rabbit

--- Client/src/main/java/com/example/demo/service/mq/rabbit/RabbitSendService.java
+++ Client/src/main/java/com/example/demo/service/mq/rabbit/RabbitSendService.java
@@ -26,7 +26,6 @@ public class RabbitSendService {
         try {
             msg += "hello rabbitmq:"+new Date();
             System.out.println("Sender:"+msg);
-            int a = 2;
             //rabbitTemplate.convertAndSend(key, msg);
             this.rabbitTemplate.convertAndSend("hello", msg);
         } catch (Exception e) {
